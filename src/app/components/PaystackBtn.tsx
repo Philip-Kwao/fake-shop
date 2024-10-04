db19cd53f2bf46183c161c49a9cd00e64f3e1339
Philip Kwao <123540619+Philip-Kwao@users.noreply.github.com>
2024-10-04 13:54:06 +0000

    Edited Onsucess prop and env

--- src/app/components/PaystackBtn.tsx
+++ src/app/components/PaystackBtn.tsx
@@ -1,34 +1,33 @@
+"use client"
+import { useRouter } from "next/navigation";
 import { PaystackButton } from "react-paystack";
 
-const config = {
-    publicKey:process.env.NEXT_PUBLIC_KEY,
-  };
-
-interface PayProp{
-    amount:number
+interface PayProp {
+  amount: number;
 }
 // console.log(process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY)
-const PaystackBtn = (prop:PayProp) => {
-    const handlePaystackSuccessAction = () => {
-        alert("Payment Successful")
-      };
-  
-      const handlePaystackCloseAction = () => {
-        console.log('closed')
-      }
-  
-      const componentProps = {
-          ...config,
-          currency:"GHS",
-          email:"bluewebergh@gmail.com",
-          amount:prop.amount*100,
-          text: 'Make Payment',
-          onSuccess: () => handlePaystackSuccessAction(),
-          onClose: handlePaystackCloseAction,
-      };
-  return (
-        <PaystackButton {...componentProps} />
-  )
-}
+const PaystackBtn = (prop: PayProp) => {
+    const router = useRouter()
+  const handlePaystackSuccessAction = () => {
+    alert("Payment Successful");
+    router.push(`/`)
 
-export default PaystackBtn
+  };
+
+  const handlePaystackCloseAction = () => {
+    console.log("closed");
+  };
+
+  const componentProps = {
+    publicKey: `${process.env.NEXT_PUBLIC_KEY}`,
+    currency: "GHS",
+    email: "bluewebergh@gmail.com",
+    amount: prop.amount * 100,
+    text: "Make Payment",
+    onSuccess: () => handlePaystackSuccessAction(),
+    onClose: handlePaystackCloseAction,
+  };
+  return <PaystackButton {...componentProps} />;
+};
+
+export default PaystackBtn;
